3c38f0773f457fba4e6485746365add3ded7fa8d
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 14:23:44 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,8 @@
+var klu1 = ["No","rumah saya", 30,"A"]
+var klu2 = ["Jalan pangeran Tirtayasa No", 70]
+
+var alamat1=klu1.join(" ")
+var alamat2=klu2.join(" ")
+
+console.log(alamat1)
+console.log(alamat2)
